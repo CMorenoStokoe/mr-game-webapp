a68af7055d01bfa2089b580aae2c13fb3b6c1952
CMorenoStokoe <CMorenoStokoe@users.noreply.github.com>
2020-06-11 11:25:34 +0100

    commititing for merge

--- algorithms/traversal.js
+++ algorithms/traversal.js
@@ -32,10 +32,73 @@ This allows a propagation network to follow its path and accurately update value
 
 */
 
+// Discover loops
+function findLoops(graph, root, recursionLimit = 10){
+    console.log('Starting Loop search, max recursions = ', recursionLimit, Math.pow(graph.numberOfNodes(), 2))
 
-// Run depth-first search to identify loops
+    // Initialise search queue
+    const queue = [{source: 'start', target: root},];
+    const visits = {};
+    const loopHolder = {source: null, target: null};
+    const loops = {};
+
+    console.log(graph, root, queue)
+    console.log('gnodes',graph.numberOfNodes())
+    
+
+    for (i = 0; i < Math.pow(graph.nodes().length, 2); i++){ // failsafe
+        
+        // Search network until queue empty
+        if(queue[0] == undefined){console.log('DFS search finished.');break;} // failsafe
+        //if(queue[0] == undefined){
+            
+            // RECORD EDGES NOT NODES, THEN KEEP COUNT OF EDGES AND ONCE TRAVELLED 10 TIMES REMOVE
+
+            // Get current node
+            const currentNode=queue[0].source;
+            const target=queue[0].target;
+            
+            console.log(currentNode);
+
+            // Shift item from queue
+            queue.shift();
+
+            // Increment node visit count
+            if(visits[currentNode]>0){ visits[currentNode]++ }else{ visits[currentNode]=1 };
+
+            // Mark node as visited
+            if(visits[currentNode] > recursionLimit){ // If node visited (n >= recursionLimit) times
+
+                console.log('looped 10', currentNode);
+
+                // Skip looping node 
+                continue;
+            }
+
+            // Add node to list of nodes in loops if visits over recursionLimit
+            if(visits[currentNode] >= recursionLimit){ 
+                loops[currentNode]=true; 
+                loopHolder.source = loopHolder.target;
+                loopHolder.target = currentNode;
+            }
+
+            // Add successors to front of queue
+            for (successor of graph.successors(currentNode)){ queue.unshift(successor); };
+
+    }
+    
+    console.log('Loop search finished.');
+    console.log('visits', visits);
+    console.log('Loops', loops);
+    console.log('Loopholder', loopHolder);
+
+    return visits;
+
+}
+
+// Run 'Two steps forward one step back" depth-first search variant
 function DFS(graph, root){
-    console.log('Starting DFS search, max iterations = ', Math.pow(graph.nodes().length, 2))
+    console.log('Starting DFS search, max iterations = ', Math.pow(graph.numberOfNodes(), 2))
 
     // Initialise queue, exhaustedNodes and path lists
     const queue = [root]; // Add root node to search queue
@@ -53,10 +116,10 @@ function DFS(graph, root){
         console.log('exhaustednodes',exhaustedNodes)
        
         // 1. Get node n from queue 
-        currentNode = queue[0];
+        const currentNode = queue[0];
 
         // 2. Check if all predecessor nodes of n have already been searched exhaustively
-        isExhausted = checkExhausted(currentNode);
+        const isExhausted = checkExhausted(currentNode);
         
         // 3. If node not exhausted, move node to back of queue 
         if(!(isExhausted)){
@@ -64,7 +127,7 @@ function DFS(graph, root){
             queue.push(currentNode);
         }
 
-        //     4. Break if a loop is found (i.e., a node searching its predecessors finds itself)
+        // 4. Break if a loop is found (i.e., a node searching its predecessors finds itself)
 
         // 5. Once a node is exhausted then add this to the path to record search order
         if(isExhausted){
