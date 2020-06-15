ba476a1122d0633b6e75525ab11106d0e5f2f736
CMorenoStokoe <CMorenoStokoe@users.noreply.github.com>
2020-06-15 11:50:07 +0100

    simplifying to no loop detection

--- algorithms/traversal.js
+++ algorithms/traversal.js
@@ -37,10 +37,10 @@ function findLoops(graph, root, recursionLimit = 10){
     console.log('Starting Loop search, max recursions = ', recursionLimit, Math.pow(graph.numberOfNodes(), 2))
 
     // Initialise search queue
-    const queue = [{source: 'start', target: root},];
+    const queue = [{source: 'start', target: root}];
     const visits = {};
-    const loopHolder = {source: null, target: null};
-    const loops = {};
+    var loopHolder = null;
+    const loops = [];
 
     console.log(graph, root, queue)
     console.log('gnodes',graph.numberOfNodes())
@@ -51,48 +51,72 @@ function findLoops(graph, root, recursionLimit = 10){
         // Search network until queue empty
         if(queue[0] == undefined){console.log('DFS search finished.');break;} // failsafe
         //if(queue[0] == undefined){
-            
-            // RECORD EDGES NOT NODES, THEN KEEP COUNT OF EDGES AND ONCE TRAVELLED 10 TIMES REMOVE
 
-            // Get current node
-            const currentNode=queue[0].source;
-            const target=queue[0].target;
+            // Current edge to search
+            const currentEdge = queue[0]; //.target node is the node currently being searched
             
-            console.log(currentNode);
+            console.log('#### searching node: ', currentEdge.target);
 
             // Shift item from queue
             queue.shift();
 
-            // Increment node visit count
-            if(visits[currentNode]>0){ visits[currentNode]++ }else{ visits[currentNode]=1 };
+            console.log('queue: ', queue);
 
-            // Mark node as visited
-            if(visits[currentNode] > recursionLimit){ // If node visited (n >= recursionLimit) times
+            // Track and mark edge visits
+            switch(visits[currentEdge]){
 
-                console.log('looped 10', currentNode);
+                case recursionLimit:// If edge at recursionLimit of visits
+                    
+                    loopHolder = currentEdge; // Store edge as previous loop in edge
+                    console.log('%cVisited 10, recording edge: [' + loopHolder.source + ',' + loopHolder.target  + ']', 'color:red');
+                    continue;
+                
+                case undefined: // If edge not visited before
+                    console.log('not visited, count=1', visits[currentEdge]);
+                    visits[currentEdge]=1; // Initialise visit count for this edge with 1
 
-                // Skip looping node 
-                continue;
+                    findSuccessors();
+                    break;
+
+                default: // If edge visited before but not at recursionLimit
+                    console.log('visited but <10, count++', visits[currentEdge]);
+                    visits[currentEdge]++; // Increment edge visit count
+
+                    findSuccessors();
+                    break;
             }
 
-            // Add node to list of nodes in loops if visits over recursionLimit
-            if(visits[currentNode] >= recursionLimit){ 
-                loops[currentNode]=true; 
-                loopHolder.source = loopHolder.target;
-                loopHolder.target = currentNode;
+            // Discover if any loop edges are the terminus
+            if (loopHolder != null){ // If there is a currently held unrecorded loop edge
+                console.log('%cif (loopHolder)', 'color: purple')
+                if (loopHolder != currentEdge){ // If current edge is not the current loop edge under investigation
+                    if (currentEdge.source != loopHolder.target){ // If previous edge does not connect to current edge
+                        console.log('%cLoop terminus found: ', 'color:green', loopHolder.source);
+                        loops.push(loopHolder.source); // Add previous edge to list of edges in loops
+                        loopHolder = null; // Reset loop holder
+                    }
+                }
             }
 
+            // Add successor nodes to search queue
+
+            function findSuccessors(){
+                for (successor of graph.successors(currentEdge.target)){ // Get all successor nodes of current node
+                    
+                    queue.unshift({ // Add dictionary entry for succeeding edge {source: current node, target: successor node}
+                        source : currentEdge.target, 
+                        target : successor
+                    }); 
+                };
+            }
             // Add successors to front of queue
-            for (successor of graph.successors(currentNode)){ queue.unshift(successor); };
+
 
     }
     
     console.log('Loop search finished.');
     console.log('visits', visits);
-    console.log('Loops', loops);
-    console.log('Loopholder', loopHolder);
-
-    return visits;
+    console.log('loops', loops);
 
 }
 
@@ -107,11 +131,12 @@ function DFS(graph, root){
 
     // Run DFS variant
     for (i = 0; i < Math.pow(graph.nodes().length, 2); i++){ 
-        if(queue[0] == undefined){console.log('DFS search finished.');break;}
-        /* Failsafes (above)
-            1. Maximum number of iterations (n nodes ^2) to avoid infinite loops 
-            2. Search while the queue is not empty  to avoid runtime error when queue is emptied
+        /* 
+        Failsafes
+            1. Maximum number of iterations (n nodes ^2) to avoid infinite loops (above)
+            2. Search while the queue is not empty  to avoid runtime error when queue is emptied (below)
         */
+        if(queue[0] == undefined){console.log('DFS search finished.');break;}
 
         // 1. Get node n from queue 
         const currentNode = queue[0];
